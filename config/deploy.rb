c10635c738f06dd1c647875d41e65a53019f0f7e
Ronan McHugh <mchugh.r@gmail.com>
2015-12-17 14:44:29 +0100

    update deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :repo_url, 'git@github.com:dtulibrary/elite.git'
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/var/www/spotlight.ddf.dtic.dk'
+set :passenger_restart_with_touch, true
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -27,7 +28,7 @@ set :linked_files, fetch(:linked_files, []).push(
 'config/database.yml', 'config/secrets.yml', 'config/blacklight.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/uploads', 'public/assets')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
