1cf2d1a8ceb7b1ee61816c0c513614c74da8dfae
Ronan McHugh <mchugh.r@gmail.com>
2015-12-18 10:20:35 +0100

    ensure user is initialized

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,8 @@ class Ability
   include Spotlight::Ability
 
   def initialize(user)
-    super
+    user ||= Spotlight::Engine.user_class.new
+    super(user)
     can :manage, [ExhibitFilter, Award],
     spotlight_exhibit_id: user.exhibit_roles.pluck(:resource_id)
   end
